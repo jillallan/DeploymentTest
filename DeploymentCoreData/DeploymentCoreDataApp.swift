b1fdaa59b178ea5988d3a7ef92da33686a3b2356
Jill Allan <54241079+jillallan@users.noreply.github.com>
2023-07-17 13:52:56 +0100

    Replace core data target

---
+++ DeploymentCoreData/DeploymentCoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeploymentCoreDataApp.swift
+//  DeploymentCoreData
+//
+//  Created by Jill Allan on 17/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct DeploymentCoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
